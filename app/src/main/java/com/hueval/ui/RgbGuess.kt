c4226660cad8ae0009cf022812bae999bfa88633
Edward Hockedy <edhockedy@hotmail.com>
2023-12-29 12:33:51 +0000

    Added initial colour generation to RGB screen

--- app/src/main/java/com/hueval/ui/RgbGuess.kt
+++ app/src/main/java/com/hueval/ui/RgbGuess.kt
@@ -1,9 +1,39 @@
 package com.hueval.ui
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import kotlin.random.Random
+
+fun getRandomColour(): Color {
+    val red = Random.nextInt(0, 255)
+    val green = Random.nextInt(0, 255)
+    val blue = Random.nextInt(0, 255)
+    return Color(red, green, blue)
+}
 
 @Composable
 fun RgbGuess() {
-    Text("RGB Guess Page")
+    var colourCode by remember { mutableStateOf(getRandomColour()) }
+
+    Canvas(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        drawRoundRect(colourCode, Offset(0F, 0F), Size(200F, 200F))
+    }
+
+    Button(onClick = { colourCode = getRandomColour() }) {
+        Text(colourCode.toString())
+    }
 }
